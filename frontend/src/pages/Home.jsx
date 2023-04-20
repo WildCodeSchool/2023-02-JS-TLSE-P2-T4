a8b589f19733a9e4f225940b07458595c93ef8c7
Almasyser <almasyser@wanadoo.fr>
2023-04-20 17:02:35 +0200

    s02- home page  first commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import "./Home.css";
+
+function Home() {
   return (
-    <header className="App-header">
-      <p>Home</p>
-    </header>
+    <section className="home-body">
+      <section className="welcome-text">
+        <p className="welcome-message">Welcome to Quizilla</p>
+        <p className="welcome-comment">
+          Test your knowledge and challenge your brain with our exiting Quiz!
+        </p>
+        <p className="welcome-choice">Try it !</p>
+      </section>
+      <section className="buttons">
+        <button type="button" className="button-settings">
+          Settings
+        </button>
+        <button type="button" className="button-start">
+          Start
+        </button>
+      </section>
+    </section>
   );
 }
+export default Home;
