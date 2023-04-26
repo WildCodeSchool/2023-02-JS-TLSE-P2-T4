650af98119594c2f50fe078dcade6ca2219a9aa0
6sous <cyrille.aquilina@gmail.com>
2023-04-26 20:27:59 +0200

    added fetch, some states, props and a function to shuffle answers

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -1,12 +1,73 @@
-function Question() {
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+function Question({ currentQuest, questions }) {
+  const [options, setOptions] = useState();
+
+  // fonction permettant de mélanger les éléments d'un tableau
+  const handleShuffle = (arr) => {
+    return arr.sort(() => Math.random() - 0.5);
+  };
+
+  // useffect permettant de mélanger les réponses afin qu'elles ne soient pas toujours dans le même ordre
+  useEffect(() => {
+    questions &&
+      setOptions(
+        handleShuffle([
+          questions[currentQuest].correct_answer,
+          ...questions[currentQuest].incorrect_answers,
+        ])
+      );
+  }, [questions, currentQuest]);
+
   return (
     <div>
       <div>
-        <h1 className="question">Question</h1>
+        <h1 className="question">{questions[currentQuest].question}</h1>
       </div>
-      <div className="answers"></div>
+      <div className="answers">
+        {options
+          ? options.map((opt) => {
+              return (
+                <button
+                  onClick={() => {}}
+                  className={`singleOption
+                    ${
+                      opt === questions[currentQuest].correct_answer
+                        ? "correct"
+                        : "wrong"
+                    }`}
+                  key={questions[currentQuest].question}
+                  type="button"
+                >
+                  {opt}
+                </button>
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 }
+
+Question.propTypes = {
+  currentQuest: PropTypes.number,
+  questions: PropTypes.arrayOf(
+    PropTypes.shape({
+      correct_answer: PropTypes.string,
+      incorrect_answers: PropTypes.string,
+      question: PropTypes.string,
+    })
+  ),
+};
+
+// 3:21  error  'currentQuest' is missing in props validation                          react/prop-types
+// 3:35  error  'questions' is missing in props validation                             react/prop-types
+// 13:5   error  Expected an assignment or function call and instead saw an expression  no-unused-expressions
+// 16:35  error  'questions[].correct_answer' is missing in props validation            react/prop-types
+// 17:38  error  'questions[].incorrect_answers' is missing in props validation         react/prop-types
+// 25:59  error  'questions[].question' is missing in props validation                  react/prop-types
+// 35:55  error  'questions[].correct_answer' is missing in props validation            react/prop-types
+// 39:48  error  'questions[].question' is missing in props validation
 
 export default Question;
--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,11 +1,50 @@
+import Question from "@components/Question";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function Quiz() {
+  // state permettant de stocker l'ensemble des questions fetch pour 1 quiz
+  const [questions, setQuestions] = useState();
+  // state pour incrémenter le score
+  const [score, setScore] = useState(0);
+  // state permettant de mettre à jour la question affichée de façon randomisée
+  const [currentQuest, setCurrentQuest] = useState(
+    Math.floor(Math.random() * 49)
+  );
+
+  // fetch des données
+  useEffect(() => {
+    axios
+      .get(
+        "https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple"
+      )
+      .then((res) => setQuestions(res.data.results))
+      .catch((err) => {
+        err.message;
+      });
+  }, []);
+
   return (
     <div className="quizContainer">
       <div className="quizHeader">
-        <div className="score"></div>
-        <div className="timer"></div>
+        <div className="score">
+          <p>Score : {score}</p>
+        </div>
+        <div className="timer" />
       </div>
-      <Question />
+
+      {questions ? (
+        <Question
+          currentQuest={currentQuest}
+          setCurrentQuest={setCurrentQuest}
+          setScore={setScore}
+          score={score}
+          questions={questions}
+          setQuestions={setQuestions}
+        />
+      ) : (
+        <p>Wait please</p>
+      )}
     </div>
   );
 }
