0dbdc962113de22cb0b38bc35cddd44559f9fd1a
6sous <cyrille.aquilina@gmail.com>
2023-04-27 15:35:51 +0200

    S3US17 edited the french variable name in Timer component

--- frontend/src/components/Timer.jsx
+++ frontend/src/components/Timer.jsx
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 function Timer() {
-  const [tempsRestant, setTempsRestant] = useState(60);
+  const [Time, setTime] = useState(60);
 
   useEffect(() => {
     // Décrémente le temps restant toutes les secondes
     const interval = setInterval(() => {
-      setTempsRestant((prevTempsRestant) =>
-        prevTempsRestant > 0 ? prevTempsRestant - 1 : 0
-      );
+      setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
     }, 1000);
 
     // Nettoie l'intervalle lorsque le composant est démonté
     return () => clearInterval(interval);
   }, []);
 
-  return <div>{tempsRestant} sec</div>;
+  return <div>{Time} sec</div>;
 }
 
 export default Timer;
