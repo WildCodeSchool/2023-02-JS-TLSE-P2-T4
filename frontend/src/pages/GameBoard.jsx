939ee31dd5327ae6a30b13dd061ccf46ba089fca
6sous <cyrille.aquilina@gmail.com>
2023-05-02 16:45:56 +0200

    S4 added smooth border to the square in the gameboard

--- frontend/src/pages/GameBoard.jsx
+++ frontend/src/pages/GameBoard.jsx
@@ -29,6 +29,7 @@ const LaunchButton = styled.button`
   background-color: black;
   border-radius: 0px;
   border: none;
+  border: 3px solid rgba(255, 255, 255, 0.2);
 
   @media (min-width: 768px) {
     position: initial;
