dd3553c51593a43489f13f14f71210ee75205d6a
6sous <cyrille.aquilina@gmail.com>
2023-05-02 22:27:53 +0200

    S4 responsive design quiz page and more

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -75,7 +75,7 @@ function Question({
 
   // ------------------------return du composant---------------------------
   return (
-    <div>
+    <div className="questionContent">
       <div>
         <h1 className="question">{questionTitle}</h1>
       </div>
