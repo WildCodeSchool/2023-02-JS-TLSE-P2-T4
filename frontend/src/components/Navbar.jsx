e66551c4076d40ed228898d694da2ae8209f8203
NGBANDAJuste-Junior <njjngbanda20@yahoo.com>
2023-05-03 21:05:02 +0200

    S4 US6 set up exit button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import "./Navbar.css";
 
-function Navbar() {
+function Navbar({ navIsVisible }) {
+  Navbar.propTypes = {
+    navIsVisible: PropTypes.bool.isRequired,
+  };
+
   const [active, setActive] = useState("navMenu");
   const [icon, setIcon] = useState("navToggler");
   const navToggle = () => {
     if (active === "navMenu") {
       setActive("navMenu navActive");
     } else setActive("navMenu");
+
+    // Icon Toggler
     if (icon === "navToggler") {
       setIcon("navToggler toggle");
     } else setIcon("navToggler");
@@ -16,24 +23,34 @@ function Navbar() {
   return (
     <nav className="nav">
       <div className="appLogo">
-        <img src="/assets/logo.png" alt="logo Quizilla" />
+        <Link to="/">
+          <img src="/assets/logo.png" alt="logo Quizilla" />
+        </Link>
       </div>
-      <ul className={active} onClick={navToggle} aria-hidden>
-        <li className="navItem">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="navItem">
-          <Link to="/rules">Rules</Link>
-        </li>
-        <li className="navItem">
-          <Link to="/about-us">About Us</Link>
-        </li>
-      </ul>
-      <button type="button" onClick={navToggle} className={icon}>
-        <div className="line1" />
-        <div className="line2" />
-        <div className="line3" />
-      </button>
+      {navIsVisible ? (
+        <>
+          <ul className={active} onClick={navToggle} aria-hidden>
+            <li className="navItem">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="navItem">
+              <Link to="/rules">Rules</Link>
+            </li>
+            <li className="navItem">
+              <Link to="/about-us">About Us</Link>
+            </li>
+          </ul>
+          <button type="button" onClick={navToggle} className={icon}>
+            <div className="line1" />
+            <div className="line2" />
+            <div className="line3" />
+          </button>
+        </>
+      ) : (
+        <Link to="/" className="exit">
+          <img src="./assets/Exit_Icon.svg" alt="icon" />
+        </Link>
+      )}
     </nav>
   );
 }
